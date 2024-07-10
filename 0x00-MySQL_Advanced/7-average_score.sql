a2592262b9cedcea5eea051ed3db40b3de202aba
adewalekujore <adewalekujore6@gmail.com>
2024-07-10 21:31:53 +0100

    update

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,9 +1,9 @@
 -- SQL script creates a stored procedure
-DELIMITER //
+DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-	DECLARE avg_score FLOAT ;
+	DECLARE avg_score FLOAT;
 	SELECT AVG(score) INTO avg_score FROM corrections c WHERE c.user_id = user_id;
 	UPDATE users SET average_score = avg_score WHERE id = user_id;
-END //
+END $$
 DELIMITER ;
